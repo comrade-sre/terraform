2c1d54079f2532f8f42bd41ea4efc7863e00a3ff
Andrei Mironov <dscjna9@gmail.com>
2019-11-24 22:11:18 +0300

    добавлена группаа безопасности для нод

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -23,6 +23,9 @@ resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = "${aws_iam_role.eks-cluster.name}"
 }
+
+
+#SG for control plane
 resource "aws_security_group" "eks-cluster" {
   name        = "terraform-eks-cluster"
   description = "Cluster communication with worker nodes"
@@ -47,17 +50,62 @@ resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
   to_port           = 443
   type              = "ingress"
 }
-#resource "aws_eks_cluster" "mentoring" {
-#  name     = var.cluster_name
-#  role_arn = "${aws_iam_role.eks-cluster.arn}"
-#
-#  vpc_config {
-#    security_group_ids = ["${aws_security_group.eks-cluster.id}"]
-#    subnet_ids         = ["${var.subnet}"]
-#  }
-#
-#  depends_on = [
-#    "aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPlicy",
-#    "aws_iam_role_policy_attachment.cluster-AmazonEKSServicePolicy"
-#  ]
-#}
+
+#Control plane init
+resource "aws_eks_cluster" "mentoring" {
+  name     = var.cluster_name
+  role_arn = "${aws_iam_role.eks-cluster.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.eks-cluster.id}"]
+    subnet_ids         = ["${var.subnet}"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPlicy",
+    "aws_iam_role_policy_attachment.cluster-AmazonEKSServicePolicy"
+  ]
+}
+
+
+#SG for workers
+resource "aws_security_group" "worker-node" {
+  name = "terraform-eks-worker-node"
+  description = "Security group for all nodes in the cluster"
+  vpc_id = var.vpc_main
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Name" = "terraform-eks-worker-node"
+    "kuberenetes.io/cluster/${var.cluster_name}" = "owned"
+    
+  }
+}
+resource "aws_security_group_rule" "worker-node-ingress-cluster" {
+  description = "Allow worker kubelets and pods to receive communication from the cluster control plane"
+  from_port = 1025
+  protocol = "tcp"
+  security_group_id = aws_security_group.worker-node.id
+  source_security_group_id = aws_security_group.eks-cluster.id
+  to_port = 65535
+  type = "ingress"
+ }
+resource "aws_security_group_rule" "demo-node-ingress-self" {
+  description = "Allow node to communicate to each other"
+  from_port = 0
+  protocol = "-1"
+  security_group_id = aws_security_group.worker-node.id
+  source_security_group_id = aws_security_group.eks-cluster.id
+  to_port = 65535
+  type = "ingress"
+}
+
+
+
+
+
