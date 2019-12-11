927c2d56472ff972c1a43e38aa142a28b886b6be
Andrei Mironov <andrei_mironov1@epam.com>
2019-12-11 13:08:10 +0300

    replace autoscaling-group by aws_eks_node_group

--- modules/eks/control_plane.tf
+++ modules/eks/control_plane.tf
@@ -54,7 +54,6 @@ resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
 resource "aws_eks_cluster" "mentoring" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks-cluster.arn
-
   vpc_config {
     security_group_ids = ["${aws_security_group.eks-cluster.id}"]
     subnet_ids         = ["${var.subnet}", "${var.subnet_2}"]
--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,9 +1,6 @@
 output "SG-id" {
   value = aws_security_group.eks-cluster.id
 }
-output "config_map_aws_auth" {
-  value = local.config_map_aws_auth
-}
 output "kubeconfig" {
   value = local.kubeconfig
 }
